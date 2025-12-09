d2269089ef0129bd9a2b6d68bed6606c1d1c6b9d
Atamacer <dan4ik-gp@yandex.ru>
2025-12-09 16:12:06 +0300

    commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# VPS DRIVE
+Для прода
+```
+git clone https://github.com/Atamacer/vps-drive
+```
+```text
+docker compose up -d -build
+```
+Для разработки
+```text
+npm install prisma --save-dev  
+```
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UploadModule } from './upload/upload.module';
+import { DownloadModule } from './download/download.module';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UploadModule],
+  imports: [
+    AuthModule,
+    UploadModule,
+    DownloadModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+  ],
   controllers: [],
   providers: [],
 })
